0783fa7042339a4430dae1971ada365f7e1d5b2f
Gabriel Dos Santos <gabriel.dossantos96@hotmail.com>
2020-02-12 19:13:19 -0300

    paste vendor

--- src/index.js
+++ src/index.js
@@ -23,10 +23,10 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "react-notification-alert/dist/animate.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 // plugins styles downloaded
-import "assets/vendor/fullcalendar/dist/fullcalendar.min.css";
-import "assets/vendor/sweetalert2/dist/sweetalert2.min.css";
-import "assets/vendor/select2/dist/css/select2.min.css";
-import "assets/vendor/quill/dist/quill.core.css";
+// import "assets/vendor/fullcalendar/dist/fullcalendar.min.css";
+// import "assets/vendor/sweetalert2/dist/sweetalert2.min.css";
+// import "assets/vendor/select2/dist/css/select2.min.css";
+// import "assets/vendor/quill/dist/quill.core.css";
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 // core styles
